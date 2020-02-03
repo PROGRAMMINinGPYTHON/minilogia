b426bc643c60bccfdc7b949884907ca4b6075edb
PROGRAMMINinGPYTHON <59664343+PROGRAMMINinGPYTHON@users.noreply.github.com>
2020-02-03 21:52:52 +0100

    kjjjjjjjj

--- pnl/test_napisy.py
+++ pnl/test_napisy.py
@@ -1,4 +1,3 @@
-
 def test_title():
     txt = "poszla ola do przedszkola"
     assert txt.title() == "Poszla Ola Do Przedszkola"
@@ -6,3 +5,30 @@ def test_title():
 def test_upper():
     txt = "poszla ola do przedszkola"
     assert txt.upper() == "POSZLA OLA DO PRZEDSZKOLA"
+
+def test_replace():
+    napis="ministerstwo"
+    assert napis.replace("i","d") == "mdndsterstwo"
+
+def test_lower():
+    txt = "AbCDefg"
+    assert txt.lower() == "abcdefg"
+    
+##def test_len():
+##    txt = len("tddd")
+##    assert txt.len(txt) == 4
+
+
+def test_len():
+    dddd = "ddd"
+    assert len(dddd) == 3
+
+    
+def test_count():
+    napis="MINISTERSTWO DO SPRAW NIEZBYT ISTOTNYCH SPRAW"
+    assert napis.count("IS") == 2
+
+
+
+def test_strip():
+    txt = 
