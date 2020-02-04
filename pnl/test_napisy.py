5cef109edebe03951cf6010201a6b2c482330690
PROGRAMMINinGPYTHON <59664343+PROGRAMMINinGPYTHON@users.noreply.github.com>
2020-02-04 19:27:55 +0100

    kdalsdkfjskdfjksajflkdsjflkdsjf'l

--- pnl/test_napisy.py
+++ pnl/test_napisy.py
@@ -13,22 +13,19 @@ def test_replace():
 def test_lower():
     txt = "AbCDefg"
     assert txt.lower() == "abcdefg"
-    
-##def test_len():
-##    txt = len("tddd")
-##    assert txt.len(txt) == 4
-
 
 def test_len():
     dddd = "ddd"
     assert len(dddd) == 3
-
-    
+   
 def test_count():
     napis="MINISTERSTWO DO SPRAW NIEZBYT ISTOTNYCH SPRAW"
     assert napis.count("IS") == 2
 
-
-
 def test_strip():
-    txt = 
+    txt ="  ddf"
+    assert txt.strip() =="ddf"
+
+def test_split():
+    txt ="MINI AUTO"
+    assert txt.split() == ['MINI' , 'AUTO']
