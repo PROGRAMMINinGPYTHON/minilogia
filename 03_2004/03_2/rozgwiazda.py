19b387cfc284e60ef0e84f66e34515582e315636
PROGRAMMINinGPYTHON <59664343+PROGRAMMINinGPYTHON@users.noreply.github.com>
2020-03-16 15:54:14 +0100

    ds

--- 03_2004/03_2/rozgwiazda.py
+++ 03_2004/03_2/rozgwiazda.py
@@ -1,10 +1,9 @@
 from turtle import *
-
 def ROZGWIAZDA(n):
-    pass
+    zegarek(n)
 
-def trojkat(kolor):
-    a = 50
+def trojkat(kolor,n):
+    a =100/n
     fillcolor(kolor)
     begin_fill()
     for i in range(3):
@@ -12,13 +11,32 @@ def trojkat(kolor):
         lt(120)
     end_fill()
 
-def gwiazda():
+def gwiazda(n):
+    a = 100/n
+    for i in range(6):
+        setheading(30+60*i)
+        trojkat("green",n)
     for i in range(6):
         setheading(90+60*i)
-        trojkat("yellowbrown")
+        fd(a)
+        rt(120)
+        trojkat("yellow",n)
+        rt(60)
+        fd(a)
         
-def zegarek():
+def zegarek(ile):
+    a = 100/ile
     for i in range(6):
         pu()
         setpos(0,0)
+        pd()
         setheading(90+60*i)
+        for j in range(ile+1):
+            gwiazda(ile)
+            setheading(90+60*i)
+            pu()
+            fd(3*a)
+            pd()
+speed(0)
+ROZGWIAZDA(10)
+
