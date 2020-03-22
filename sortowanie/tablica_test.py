0de35e5191ba4535ee6105095182369bda14372c
PROGRAMMINinGPYTHON <59664343+PROGRAMMINinGPYTHON@users.noreply.github.com>
2020-03-22 11:31:00 +0100

    sortowanie 10zł

--- sortowanie/tablica_test.py
+++ sortowanie/tablica_test.py
@@ -1,4 +1,5 @@
 from sortowanie import *
+import itertools
 
 def test_sortowanie():
     tab = [3, 1]
@@ -15,10 +16,10 @@ def test_sortowanie_3():
     sortowanie(tab)
     assert tab == [1,2,3,4,5,6,7,8]
 
-def test_failed():
-    tab =[1,2,4,3]
-    sortowanie(tab)
-    assert tab == [1,2,4,3]
+##def test_failed():
+##    tab =[1,2,4,3]
+##    sortowanie(tab)
+##    assert tab == [1,2,4,3]
 
 def test_5():
     tab=[10,9,8,7,6,5,4,3,2,1]
@@ -30,3 +31,25 @@ def test_4():
     sortowanie(tab)
     assert tab == []
 
+def test_6():
+    tab = [1,2,3,4,5]
+    sortowanie(tab)
+    assert tab == [1,2,3,4,5]
+
+def test_7():
+    tab = [5,2,3,4,9]
+    print(tab)
+    sortowanie(tab)
+    print (tab)
+    assert tab == [2,3,4,5,9]
+    
+def test_kombinacje():
+    sorted = [1,2,3,4,5,6]
+    all = list(itertools.permutations(sorted))
+    for tuple in all:
+        tab = list(tuple)
+        sortowanie(tab)
+        assert tab == sorted
+
+        
+    
