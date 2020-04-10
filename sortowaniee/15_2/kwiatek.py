e35c0d32a6c2e3fac078538d190b5883409b868c
PROGRAMMINinGPYTHON <59664343+PROGRAMMINinGPYTHON@users.noreply.github.com>
2020-04-10 17:42:56 +0200

    niedokonczone organy

--- sortowaniee/15_2/kwiatek.py
+++ sortowaniee/15_2/kwiatek.py
@@ -1,2 +1,38 @@
 from turtle import *
+global a
+a = 22
+def pieciokąt():
+    fillcolor("skyblue")
+    begin_fill()
+    for i in range(5):
+        fd(a)
+        rt(360/5)
+    end_fill()
 
+def koło():
+    for i in range(10):
+        pieciokąt()
+        for i in range(2):
+            fd(a)
+            rt(360/5)
+        rt(180)
+        rt(360/5)
+
+def kwiat():
+    pu()
+    setpos(100,50)
+    pd()
+    setheading(180-90)
+    for i in range(10):
+        pd()
+        koło()
+        for j in range(4):
+            pu()
+            fd(a)
+            lt(360/10)
+        lt(360/5)
+        lt(180)
+
+speed(0)
+kwiat()
+done()
--- sortowaniee/15_2/posadzka.py
+++ sortowaniee/15_2/posadzka.py
@@ -1,1 +1,55 @@
-from turle import *
+from turtle import *
+from random import *
+
+def kwadrat(ile):
+    a = 480/(ile*3)+1
+    kolory = ["red","green","yellow","black","pink","skyblue","brown","yellowgreen","darkgreen","orange","grey","white","purple"]
+    fillcolor(kolory[randrange(13)])
+    begin_fill()
+    for i in range(4):
+        fd(a)
+        rt(90)
+    end_fill()
+
+def rządek(ile):
+    a = 480/(ile*3)+1
+    for i in range(ile+1):
+        kwadrat(ile)
+        fd(a/2)
+        lt(90)
+        fd(1)
+        if i > ile-1:
+            color("white")
+        fd(2*a)
+        lt(90)
+        fd(a/2)
+        rt(90)
+        fd(a)
+        rt(90)
+    color("black")
+
+def kolumny(ile):
+    a = 480/(ile*3)+1
+    for i in range(ile):
+        pu()
+        setpos((-480/2)+1.5*a+(3*a*i),-480/2)
+        setheading(0)
+        pd()
+        rządek(ile)
+
+def linie(ile):
+    a = 480/(ile*3)+1
+    for i in range(ile):
+        pu()
+        setpos(480/2,(-480/2)+1.5*a+(3*a*i)-1*a)
+        pd()
+        setheading(90)
+        pd()
+        rządek(ile)
+
+def posadzka(ile):
+    linie(ile)
+    kolumny(ile)
+speed(0)
+posadzka(3)
+done()
